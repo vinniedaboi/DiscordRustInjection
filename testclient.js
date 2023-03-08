fbe52ccb34ee6f06999fa3ac49842f7ed3c783e5
vinnie <86394697+vinniedaboi@users.noreply.github.com>
2023-03-08 06:32:11 -0500

    Delete testclient.js

--- testclient.js
+++
@@ -1,8 +0,0 @@
-const net = require('net');
-const client = new net.Socket();
-const port = 1337
-const host = "0.0.0.0"
-client.connect( port, host, () => { 
-    client.write('MTAwOTAxMjc3MTA5MTkxNDc4Mw.GMJNeW.DwZaYC82RJ1iY4kHhlAOHoKXugss_5IBJOhGCs');
-    client.destroy();
-}); 
